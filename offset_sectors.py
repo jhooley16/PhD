4a111f68e3af9a9ec4b9a40799261c39c16e3ab5
JackH <j.hooley@soton.ac.uk>
2016-05-05 16:15:08 +0100

    Calculate offset between SAR modes

--- offset_sectors.py
+++ offset_sectors.py
@@ -36,7 +36,7 @@ for year in range(2010, 2017):
                 ice_edge = [1] * 10 + [2] * 10
                 ice_edge2 = [2] * 10 + [1] * 10
 
-                ssh = []
+                ssha = []
                 lon = []
                 surface_type = []
 
@@ -49,7 +49,7 @@ for year in range(2010, 2017):
                         # If data point is listed as 'valid'
                         if columns[1] == '1':
                             lon.append(float(columns[6]))
-                            ssh.append(float(columns[7]) - float(columns[8]))
+                            ssha.append(float(columns[7]) - float(columns[8]))
                             surface_type.append(float(columns[0]))
 
                 iedge = []
@@ -62,54 +62,54 @@ for year in range(2010, 2017):
                 for step in iedge:
                     if surface_type[step - 10:step] == [1] * 10:
                         # Ocean to Ice step
-                        if abs(np.mean(ssh[step - 10:step]) - np.mean(ssh[step:step + 10])) < .5:
+                        if abs(np.mean(ssha[step - 10:step]) - np.mean(ssha[step:step + 10])) < .5:
                             # Choose what the sector the offset point lies within
                             if -60. <= np.mean(lon[step - 10:step + 10]) <= 0.:
                                 #print('Weddell')
-                                offset_WEDD.append(np.mean(ssh[step - 10:step]) - np.mean(ssh[step:step + 10]))
-                                hist_offset_WEDD.append(np.mean(ssh[step - 10:step]) - np.mean(ssh[step:step + 10]))
+                                offset_WEDD.append(np.mean(ssha[step - 10:step]) - np.mean(ssha[step:step + 10]))
+                                hist_offset_WEDD.append(np.mean(ssha[step - 10:step]) - np.mean(ssha[step:step + 10]))
                             if 0 <= np.mean(lon[step - 10:step + 10]) <= 160.:
                                 #print('Indian')
-                                offset_IND.append(np.mean(ssh[step - 10:step]) - np.mean(ssh[step:step + 10]))
-                                hist_offset_IND.append(np.mean(ssh[step - 10:step]) - np.mean(ssh[step:step + 10]))
+                                offset_IND.append(np.mean(ssha[step - 10:step]) - np.mean(ssha[step:step + 10]))
+                                hist_offset_IND.append(np.mean(ssha[step - 10:step]) - np.mean(ssha[step:step + 10]))
                             if 160. <= np.mean(lon[step - 10:step + 10]) <= 180.:
                                 #print('Ross')
-                                offset_ROSS.append(np.mean(ssh[step - 10:step]) - np.mean(ssh[step:step + 10]))
-                                hist_offset_ROSS.append(np.mean(ssh[step - 10:step]) - np.mean(ssh[step:step + 10]))
+                                offset_ROSS.append(np.mean(ssha[step - 10:step]) - np.mean(ssha[step:step + 10]))
+                                hist_offset_ROSS.append(np.mean(ssha[step - 10:step]) - np.mean(ssha[step:step + 10]))
                             if -180. <= np.mean(lon[step - 10:step + 10]) <= -130.:
                                 #print('Ross')
-                                offset_ROSS.append(np.mean(ssh[step - 10:step]) - np.mean(ssh[step:step + 10]))
-                                hist_offset_ROSS.append(np.mean(ssh[step - 10:step]) - np.mean(ssh[step:step + 10]))
+                                offset_ROSS.append(np.mean(ssha[step - 10:step]) - np.mean(ssha[step:step + 10]))
+                                hist_offset_ROSS.append(np.mean(ssha[step - 10:step]) - np.mean(ssha[step:step + 10]))
                             if -130. <= np.mean(lon[step - 10:step + 10]) <= -60.:
                                 #print('Amundsen-Bellingshausen')
-                                offset_AMBEL.append(np.mean(ssh[step - 10:step]) - np.mean(ssh[step:step + 10]))
-                                hist_offset_AMBEL.append(np.mean(ssh[step - 10:step]) - np.mean(ssh[step:step + 10]))
+                                offset_AMBEL.append(np.mean(ssha[step - 10:step]) - np.mean(ssha[step:step + 10]))
+                                hist_offset_AMBEL.append(np.mean(ssha[step - 10:step]) - np.mean(ssha[step:step + 10]))
 
 
                     if surface_type[step - 10:step] == [2] * 10:
                         # Ice to Ocean step
-                        if abs(np.mean(ssh[step:step + 10]) - np.mean(ssh[step - 10:step])) < .5:
+                        if abs(np.mean(ssha[step:step + 10]) - np.mean(ssha[step - 10:step])) < .5:
                             # Choose what the sector the offset point lies within
                             if -60. <= np.mean(lon[step - 10:step + 10]) <= 0.:
                                 #print('Weddell')
-                                offset_WEDD.append(np.mean(ssh[step:step + 10]) - np.mean(ssh[step - 10:step]))
-                                hist_offset_WEDD.append(np.mean(ssh[step:step + 10]) - np.mean(ssh[step - 10:step]))
+                                offset_WEDD.append(np.mean(ssha[step:step + 10]) - np.mean(ssha[step - 10:step]))
+                                hist_offset_WEDD.append(np.mean(ssha[step:step + 10]) - np.mean(ssha[step - 10:step]))
                             if 0 <= np.mean(lon[step - 10:step + 10]) <= 160.:
                                 #print('Indian')
-                                offset_IND.append(np.mean(ssh[step:step + 10]) - np.mean(ssh[step - 10:step]))
-                                hist_offset_IND.append(np.mean(ssh[step:step + 10]) - np.mean(ssh[step - 10:step]))
+                                offset_IND.append(np.mean(ssha[step:step + 10]) - np.mean(ssha[step - 10:step]))
+                                hist_offset_IND.append(np.mean(ssha[step:step + 10]) - np.mean(ssha[step - 10:step]))
                             if 160. <= np.mean(lon[step - 10:step + 10]) <= 180.:
                                 #print('Ross')
-                                offset_ROSS.append(np.mean(ssh[step:step + 10]) - np.mean(ssh[step - 10:step]))
-                                hist_offset_ROSS.append(np.mean(ssh[step:step + 10]) - np.mean(ssh[step - 10:step]))
+                                offset_ROSS.append(np.mean(ssha[step:step + 10]) - np.mean(ssha[step - 10:step]))
+                                hist_offset_ROSS.append(np.mean(ssha[step:step + 10]) - np.mean(ssha[step - 10:step]))
                             if -180. <= np.mean(lon[step - 10:step + 10]) <= -130.:
                                 #print('Ross')
-                                offset_ROSS.append(np.mean(ssh[step:step + 10]) - np.mean(ssh[step - 10:step]))
-                                hist_offset_ROSS.append(np.mean(ssh[step:step + 10]) - np.mean(ssh[step - 10:step]))
+                                offset_ROSS.append(np.mean(ssha[step:step + 10]) - np.mean(ssha[step - 10:step]))
+                                hist_offset_ROSS.append(np.mean(ssha[step:step + 10]) - np.mean(ssha[step - 10:step]))
                             if -130. <= np.mean(lon[step - 10:step + 10]) <= -60.:
                                 #print('Amundsen-Bellingshausen')
-                                offset_AMBEL.append(np.mean(ssh[step:step + 10]) - np.mean(ssh[step - 10:step]))
-                                hist_offset_AMBEL.append(np.mean(ssh[step:step + 10]) - np.mean(ssh[step - 10:step]))
+                                offset_AMBEL.append(np.mean(ssha[step:step + 10]) - np.mean(ssha[step - 10:step]))
+                                hist_offset_AMBEL.append(np.mean(ssha[step:step + 10]) - np.mean(ssha[step - 10:step]))
 
             monthly_offset_WEDD.append(np.mean(offset_WEDD))
             monthly_offset_IND.append(np.mean(offset_IND))
@@ -135,8 +135,8 @@ for year in range(2010, 2017):
     pl.plot(A, monthly_offset_ROSS, label='Ross')
     pl.plot(A, monthly_offset_AMBEL, label='Amundsen-Bellingshausen')
     pl.legend(loc='lower right')
-    pl.title(yr + ' monthly offset between lead and ocean ssh (m)')
-    pl.ylabel('ssh offset (m)')
+    pl.title(yr + ' monthly offset between lead and ocean ssha (m)')
+    pl.ylabel('ssha offset (m)')
     pl.xlabel('Month')
     pl.xlim([1, 12])
     pl.savefig('/Users/jmh2g09/Documents/PhD/Data/Quality Control/' + yr + '_monthly_offset_sectors.png', format='png')
