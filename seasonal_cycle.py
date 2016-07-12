745e9111a7f8e3a38b61f524fd1e2ec5d7da82e9
JackH <j.hooley@soton.ac.uk>
2016-07-12 13:38:48 +0100

    Calculate seasonal cycle of DOT

---
+++ seasonal_cycle.py
@@ -0,0 +1,128 @@
+import numpy as np
+import os
+import matplotlib.pyplot as pl
+from netCDF4 import Dataset
+import functions as funct
+from mpl_toolkits.basemap import Basemap
+
+# Calculate the average DOT anomaly for every month, resulting in the seasonal cycle 
+
+# Cycle through the years and months
+circumpolar_anomaly_cycle = np.zeros(12)
+circumpolar_under_ice_cycle = np.zeros(12)
+circumpolar_open_ocean_cycle = np.zeros(12)
+count = np.zeros(12)
+
+timeseries = []
+timeseries_ice = []
+timeseries_ocean = []
+
+total_area_ice = []
+total_area_ocean = []
+total_area_all = []
+
+
+for year in ['2010', '2011', '2012', '2013', '2014', '2015', '2016']:
+    os.chdir('/Users/jmh2g09/Documents/PhD/Data/Gridded/DOT/' + year + '/Anomalies')
+    for month in ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12']:
+        month_index = int(month) - 1
+        # Open the month file 
+        # If the file exists...
+        file = year + month + '_DOT_anomaly.nc'
+        if os.path.isfile(file):
+            # Extract the dot anomaly
+            nc = Dataset(file, 'r')
+            lat = nc.variables['latitude'][:]
+            lon = nc.variables['longitude'][:]
+            # Need to load the DOT three times, to avoid calculations crossing over
+            dot_anom  = nc.variables['dynamic_ocean_topography_anomaly'][:]
+            dot_anom_ice  = nc.variables['dynamic_ocean_topography_anomaly'][:]
+            dot_anom_ocean = nc.variables['dynamic_ocean_topography_anomaly'][:]
+            ice_conc = nc.variables['sea_ice_concentration'][:]
+            nc.close()
+
+            
+            ## Calculate the surface area of each cell
+            # Mesh the lat and lon together calculate the surface area for each cell
+            grid_lon, grid_lat = np.meshgrid(lon, lat)
+            # Calculate the surface area of each cell
+            S = funct.surface_area(grid_lat, grid_lon, 0.5, 1.0)
+            
+            ## Get circumpolar average for all ocean for this month
+            # Get total area for the ocean region
+            total_area = np.nansum(np.nansum(~np.isnan(dot_anom) * S))
+            total_area_all.append(total_area)
+            # Calculate the weighted average DOT for the whole Southern Ocean
+            timeseries.append(np.nansum(dot_anom * S) / total_area)
+            circumpolar_anomaly_cycle[month_index] += np.nansum(dot_anom * S) / total_area
+
+            ## Calculate under ice average for this month
+            # Make open ocean and land grid cells NaNs
+            open_ocean = np.where(ice_conc == 0)
+            for i in range(np.shape(open_ocean)[1]):
+                dot_anom_ice[open_ocean[0][i]][open_ocean[1][i]] = np.nan         
+            # Calculate the total area covered by sea ice
+            ice_area = np.nansum(np.nansum(~np.isnan(dot_anom_ice) * S))
+            total_area_ice.append(ice_area)
+            # Calculate the weighted average DOT under the sea ice
+            circumpolar_under_ice_cycle[month_index] += np.nansum(dot_anom_ice * S) / ice_area
+            timeseries_ice.append(np.nansum(dot_anom_ice * S) / ice_area)
+            
+            ## Calculate under ice average for this month
+            # Make open ocean and land grid cells NaNs
+            under_ice = np.where(ice_conc > 0)
+            for i in range(np.shape(under_ice)[1]):
+                dot_anom_ocean[under_ice[0][i]][under_ice[1][i]] = np.nan
+            # Calculate the total area covered by sea ice
+            ocean_area = np.nansum(np.nansum(~np.isnan(dot_anom_ocean) * S))
+            total_area_ocean.append(ocean_area)
+            # Calculate the weighted average DOT under the sea ice
+            circumpolar_open_ocean_cycle[month_index] += np.nansum(dot_anom_ocean * S) / ocean_area
+            timeseries_ocean.append(np.nansum(dot_anom_ocean * S) / ocean_area)
+
+            #test.append((np.nansum(dot_anom_ocean * S) + np.nansum(dot_anom_ice * S)) / total_area)
+            count[month_index] += 1
+            
+            # Cycle through the longitudes, and average the sectors
+            #for ilon in np.arange(len(lon)):
+             #   if lon[ilon] > 300.:
+              #      #print('Weddell')
+               #     #weddell_anomaly_cycle[month_index] =+ np.nanmean(dot_anom[ilon, :])
+                #    pass
+circumpolar_anomaly_cycle /= count
+circumpolar_under_ice_cycle /= count
+circumpolar_open_ocean_cycle /= count
+
+pl.figure()
+pl.plot(total_area_all, label='Total Ocean')
+pl.plot(total_area_ice, label='Under Ice')
+pl.plot(total_area_ocean, label='Open Ocean')
+pl.legend(loc='best')
+pl.xlabel('Month')
+pl.ylabel('Total surface area (km$^2$)')
+pl.savefig('../../../../Seasonal/total_ice_area.png', format='png', dpi=300)
+pl.close()
+
+pl.figure()
+pl.plot(np.arange(1, 13), circumpolar_anomaly_cycle, label='Total Ocean')
+pl.plot(np.arange(1, 13), circumpolar_under_ice_cycle, label='Under Ice')
+pl.plot(np.arange(1, 13), circumpolar_open_ocean_cycle, label='Open Ocean')
+pl.legend()
+pl.xlabel('Month')
+pl.ylabel('Dynamic Ocean Topography Anomaly (m)')
+pl.xlim([1, 12])
+pl.savefig('../../../../Seasonal/seasonal_cycle.png', format='png', dpi=300)
+pl.close()
+
+pl.figure()
+pl.plot(timeseries, label='Total Ocean')
+pl.plot(timeseries_ice, label='Under Ice')
+pl.plot(timeseries_ocean, label='Open Ocean')
+pl.legend(loc='upper left')
+pl.xlabel('Month')
+pl.ylabel('Dynamic Ocean Topography Anomaly (m)')
+pl.savefig('../../../../Seasonal/seasonal_timeseries.png', format='png', dpi=300)
+pl.close()
+
+
+
